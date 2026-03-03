cfa1d686f35e27a1cf4a49b0c342c1d1b63227e0
alrago24 <alinson_04@hotmail.com>
2026-03-02 21:14:10 -0500

    feat: Se implementa funcion para  inicio de sesión y función para cargar notas de cada  materia

--- Archivo.py
+++ Archivo.py
@@ -22,8 +22,8 @@ while True:
         cantidadMaterias = int(input("¿Cuántas materias vas a ver?: "))
         
         materiasUsuario = {}
-        for int in range(cantidadMaterias):
-            nombreMateria = input(f"Nombre de la materia {int+1}: ")
+        for i in range(cantidadMaterias):
+            nombreMateria = input(f"Nombre de la materia {i+1}: ")
             materiasUsuario[nombreMateria] = []
             
         nuevoUsuario = {
@@ -40,3 +40,32 @@ while True:
         correoUsuario = input("Ingresa tu correo registrado: ")
         contraseñaUsuario = input("Ingresa tu contraseña: ")
 
+        usuarioEcontrado = None
+        
+        for usuario in usuarios:
+            if usuario["email"] == correoUsuario and usuario["password"] == contraseñaUsuario:
+                usuarioEcontrado = usuario
+        
+        if usuarioEcontrado:
+            print(f"\n¡Hola de nuevo, {usuarioEcontrado['nombre']}!")
+            
+            while True:
+                print("\n¿Qué deseas hacer?")
+                print("1. Cargar notas (3 por materia)")
+                print("2. Ver promedios y reporte")
+                print("3. Cerrar sesión")
+                
+                opcionEstudiante = input("Selecciona una opción: ")
+                
+                if opcionEstudiante == "1":
+
+                    for nombreMateria in usuarioEcontrado["materias"]:
+                        print(f"\nNotas para: {nombreMateria}")
+                        listaDeNotas = []
+
+                        for n in range(3):
+                            nota = float(input(f"  Ingresa la nota {n+1}: "))
+                            listaDeNotas.append(nota)
+
+                        usuarioEcontrado["materias"][nombreMateria] = listaDeNotas
+                    print("\n¡Todas las notas han sido cargadas!")
